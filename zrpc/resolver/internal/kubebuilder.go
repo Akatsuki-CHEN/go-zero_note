12d602e91a6836d271e3141e88fd4bfae1a90df8
关永琛 <yongchenguan@gmail.com>
2025-08-01 14:50:48 +0800

    feat: 基于k8s的endpoints服务发现

--- zrpc/resolver/internal/kubebuilder.go
+++ zrpc/resolver/internal/kubebuilder.go
@@ -44,21 +44,25 @@ type kubeBuilder struct{}
 
 func (b *kubeBuilder) Build(target resolver.Target, cc resolver.ClientConn,
 	_ resolver.BuildOptions) (resolver.Resolver, error) {
+	// 解析k8s的target
 	svc, err := kube.ParseTarget(target)
 	if err != nil {
 		return nil, err
 	}
 
+	// 获取k8s的集群配置
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
 	}
 
+	// 创建k8s的客户端
 	cs, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
 
+	// 当服务端口未指定时，从 Kubernetes Endpoints 中获取实际的服务端口信息
 	if svc.Port == 0 {
 		// getting endpoints is only to get the port
 		endpoints, err := cs.CoreV1().Endpoints(svc.Namespace).Get(
@@ -70,21 +74,32 @@ func (b *kubeBuilder) Build(target resolver.Target, cc resolver.ClientConn,
 		svc.Port = int(endpoints.Subsets[0].Ports[0].Port)
 	}
 
-	handler := kube.NewEventHandler(func(endpoints []string) {
-		endpoints = subset(endpoints, subsetSize)
-		addrs := make([]resolver.Address, 0, len(endpoints))
-		for _, val := range endpoints {
-			addrs = append(addrs, resolver.Address{
-				Addr: fmt.Sprintf("%s:%d", val, svc.Port),
-			})
-		}
+	handler := kube.NewEventHandler(
+		// 这个方法是：将新的服务器地址列表更新到 gRPC 客户端，这样客户端就知道可以连接到哪些服务器实例
+		func(endpoints []string) {
+			/* 优化：
+			在大多数情况下，客户端并不需要知道所有的服务实例：
+			gRPC 客户端通常只需要少数几个健康的实例即可
+			负载均衡算法可以在较小的子集中实现良好的负载分布
+			通过随机打乱后选择前 32 个实例（subsetSize = 32），可以确保选择的实例具有随机性和代表性
+			*/
+			endpoints = subset(endpoints, subsetSize)
+			addrs := make([]resolver.Address, 0, len(endpoints))
+			for _, val := range endpoints {
+				addrs = append(addrs, resolver.Address{
+					Addr: fmt.Sprintf("%s:%d", val, svc.Port),
+				})
+			}
 
-		if err := cc.UpdateState(resolver.State{
-			Addresses: addrs,
-		}); err != nil {
-			logx.Error(err)
-		}
-	})
+			if err := cc.UpdateState(resolver.State{
+				Addresses: addrs,
+			}); err != nil {
+				logx.Error(err)
+			}
+		},
+	)
+
+	// 用于监听 Kubernetes 中的 Endpoints 资源变化
 	inf := informers.NewSharedInformerFactoryWithOptions(cs, resyncInterval,
 		informers.WithNamespace(svc.Namespace),
 		informers.WithTweakListOptions(func(options *v1.ListOptions) {
@@ -96,6 +111,7 @@ func (b *kubeBuilder) Build(target resolver.Target, cc resolver.ClientConn,
 		return nil, err
 	}
 
+	// 这里拿到的是Pod的地址而不是 Service 的 ClusterIP
 	// get the initial endpoints, cannot use the previous endpoints,
 	// because the endpoints may be updated before/after the informer is started.
 	endpoints, err := cs.CoreV1().Endpoints(svc.Namespace).Get(
